a800a13b83bd3090bd5e85d8f35de0223161d93f
Alexandra Sazhin <alexandra@sazhin.us>
2019-12-03 00:00:51 -0500

    basic display fin

--- src/PopUp.java
+++ src/PopUp.java
@@ -1,25 +1,52 @@
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.Popup;
 import javax.swing.PopupFactory;
 
-public class PopUp {
+public class PopUp extends Popup{
 	Popup pop;
 	
-	public PopUp() {
+	public PopUp(Deal[] dealArray) {
 		PopupFactory poper = new PopupFactory();
 		
-		JFrame frame = new JFrame("SearchForDeals");
+		//basic panel
+		JFrame frame = new JFrame("You have new deals available!");
 		JLabel label = new JLabel("Deals Available!");
 		
-		frame.setSize(100, 300);
+		frame.setSize(1500, 600);
 		JPanel panel = new JPanel();
 		
 		panel.add(label);
-		pop = poper.getPopup(frame, panel, 100, 50);
+		pop = poper.getPopup(frame, panel, 100, 100);
 		
 		frame.add(panel);
+		
+		//list
+		String[] links = new String[dealArray.length];
+		String[] names = new String[dealArray.length];
+		JPanel[] panelList = new JPanel[dealArray.length];
+		
+		for(int i = 0; i < dealArray.length; i++) {
+			links[i] = dealArray[i].getLink();
+			names[i] = dealArray[i].getName();
+			
+			JLabel nameLabel = new JLabel(dealArray[i].getName());
+			JLabel linkLabel = new JLabel(dealArray[i].getLink());
+		}
+		
+		JPanel listPanel = new JPanel();
+		BoxLayout layout = new BoxLayout(listPanel, BoxLayout.X_AXIS);
+		
+		JList listLinks = new JList(links);
+		JList listNames = new JList(names);
+		listPanel.add(listNames);
+		listPanel.add(listLinks);
+		frame.add(listPanel);
+		
+		
 		frame.show();
 	}
 	
