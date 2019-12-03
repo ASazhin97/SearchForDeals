54f5ad0a32282840edf6b74f5279d26435f3c0ae
Thang Dao <dao.thang99@gmail.com>
2019-12-02 19:00:15 -0500

    fixed link parser

--- src/WebPageParser.java
+++ src/WebPageParser.java
@@ -4,37 +4,33 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebPageParser {
 	Document doc;
+	List<String> URLdeal = new ArrayList<>();
+
 	public WebPageParser(String page) {
-		// This gets all the links from the webpage.
 		try {
-			doc = Jsoup.connect("https://dealsea.com/").get();
-			System.out.println(doc.title());
-			String title = doc.title();
-			System.out.println("Title: " + title);
+			// format of URL is: https://dealsea.com/search?q=" + keyword + "&search_mode=Deals
+			doc = Jsoup.connect("https://dealsea.com/search?q=router&search_mode=Deals").get();
+			System.out.println("SEARCHING FOR DEALS...");
+			String keyword = doc.title();
+			String[] text = keyword.split(" ");
+			System.out.println("KEYWORD: " + text[1]);
 
 			Elements links = doc.select("a[href]");
 			for (Element link : links) {
-				System.out.println("link: " + link.attr("href"));
-				System.out.println("text: " + link.text());
+				if (link.text().toLowerCase().contains("router")) {
+					System.out.println("Discounted Item: " + link.text());
+					System.out.println("Link: " + link.absUrl("href"));
+					URLdeal.add(link.absUrl("href"));
+				}
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-	
-	public void getAllDeals() {
-		
-	}
-
-	public void search(String search) throws IOException {
-		String url = "https://dealsea.com/search?q=" + search + "&search_mode=Deals";
-		doc = Jsoup.connect(url).userAgent("Jsoup client").timeout(5000).get();
-		
-
 	}
 }
