0c8b519e37f72c135f6d705557ad8725c319c19c
Thang Dao <dao.thang99@gmail.com>
2019-12-03 15:41:58 -0500

    fixed

--- src/WebPageParser.java
+++ src/WebPageParser.java
@@ -9,34 +9,19 @@ import java.util.List;
 
 public class WebPageParser {
 	private Document doc;
+	private List<String> URLdeal = new ArrayList<>();
 	public static Handler handler;
 	ArrayList<Deal> _deals;
 
 	public WebPageParser(Handler handler) {
 		try {
-<<<<<<< HEAD
-			if (handler.getKeyWords().contains(" ")) {
-				String newKeyword = handler.getKeyWords().replace(" ", "+");
-				doc = Jsoup.connect("https://dealsea.com/search?q=" + newKeyword + "&search_mode=Deals").get();
-			} else {
-				doc = Jsoup.connect("https://dealsea.com/search?q=" + handler.getKeyWords() + "&search_mode=Deals").get();
-			}
-=======
-			
->>>>>>> addMultSearch
+
 			System.out.println("SEARCHING FOR DEALS...");
 			System.out.println("KEYWORD: " + handler.getKeyWords() + " ");
 			_deals = new ArrayList<Deal>();
-			
+
 			String[] wordsArray = handler.getKeyWords().split(", ");
 
-<<<<<<< HEAD
-			Elements links = doc.select("a[href]");
-			for (Element link : links) {
-				if (link.text().toLowerCase().contains(handler.getKeyWords())) {
-					Deal newDeal = new Deal(link.text(), link.absUrl("href"));
-					_deals.add(newDeal);
-=======
 
 			for (String s : wordsArray) {
 				System.out.println(s);
@@ -49,18 +34,17 @@ public class WebPageParser {
 						Deal newDeal = new Deal(link.text(), link.absUrl("href"));
 						_deals.add(newDeal);
 					}
->>>>>>> addMultSearch
 				}
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public ArrayList<Deal> getNewDeals(){
 		return _deals;
 	}
-	
+
 }
